ef035398ade037da376de78db2991296a5d75e7a
Adri <apereg24@estudiantes.unileon.es>
2020-03-27 18:35:24 +0100

    Metodos sencillos

--- ed-a003-2020/src/ule/edi/doubleLinkedList/DoubleLinkedListImpl.java
+++ ed-a003-2020/src/ule/edi/doubleLinkedList/DoubleLinkedListImpl.java
@@ -6,45 +6,41 @@ import java.util.NoSuchElementException;
 import ule.edi.exceptions.EmptyCollectionException;
 
 public class DoubleLinkedListImpl<T> implements DoubleList<T> {
-	
-	
-	//	referencia al primer nodo de la lista
+
+	// referencia al primer nodo de la lista
 	private DoubleNode<T> front;
-	
-	//	referencia al Último nodo de la lista
+
+	// referencia al Último nodo de la lista
 	private DoubleNode<T> last;
-	
-	
+
 	private class DoubleNode<T> {
-		
+
 		DoubleNode(T element) {
 			this.elem = element;
 			this.next = null;
 			this.prev = null;
 		}
-		
+
 		T elem;
-		
+
 		DoubleNode<T> next;
-	    DoubleNode<T> prev;
+		DoubleNode<T> prev;
 	}
 
 ///// ITERADOR normal //////////
 
 	@SuppressWarnings("hiding")
 	private class DobleLinkedListIterator<T> implements Iterator<T> {
-		  // añadir atributos
-	 
-		
-       	
+		// añadir atributos
+
 		public DobleLinkedListIterator(DoubleNode<T> nodo) {
 			// todo
-			
-				}
-		
+
+		}
+
 		@Override
 		public boolean hasNext() {
-			
+
 			// todo
 			return false;
 		}
@@ -54,28 +50,23 @@ public class DoubleLinkedListImpl<T> implements DoubleList<T> {
 			// todo
 
 			return null;
-			
-				}
 
-		
+		}
+
 	}
-	
+
 	////// FIN ITERATOR
-	
-	
-	
-	/// TODO :  AÑADIR OTRAS CLASES PARA LOS OTROS 3 ITERADORES
-	
-	
-    /////
-	
+
+	/// TODO : AÑADIR OTRAS CLASES PARA LOS OTROS 3 ITERADORES
+
+	/////
+
 	@SafeVarargs
-	public DoubleLinkedListImpl(T...v ) {
-		for (T elem:v) {
+	public DoubleLinkedListImpl(T... v) {
+		for (T elem : v) {
 			this.insertLast(elem);
 		}
 	}
-
 
 	@Override
 	public boolean isEmpty() {
@@ -83,55 +74,47 @@ public class DoubleLinkedListImpl<T> implements DoubleList<T> {
 		return false;
 	}
 
-
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		this.front = null;
+		this.last = null;
 	}
-
 
 	@Override
 	public void insertFirst(T elem) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	@Override
 	public void insertLast(T elem) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-
 	@Override
-	public T removeFirst() throws EmptyCollectionException{
+	public T removeFirst() throws EmptyCollectionException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-
 	@Override
-	public T removeLast()  throws EmptyCollectionException{
+	public T removeLast() throws EmptyCollectionException {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 
 	@Override
 	public void insertPos(T elem, int position) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	@Override
 	public void insertBefore(T elem, T target) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	@Override
 	public T getElemPos(int position) {
@@ -139,13 +122,11 @@ public class DoubleLinkedListImpl<T> implements DoubleList<T> {
 		return null;
 	}
 
-
 	@Override
 	public int getPosFirst(T elem) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
 
 	@Override
 	public int getPosLast(T elem) {
@@ -153,39 +134,116 @@ public class DoubleLinkedListImpl<T> implements DoubleList<T> {
 		return 0;
 	}
 
-
 	@Override
 	public T removePos(int pos) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-
 	@Override
 	public int removeAll(T elem) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+		/*
+		 * Si el elemento a eliminar es nulo se lanza la excepcion marcada en la
+		 * documentacion
+		 */
+		if (elem == null)
+			throw new NullPointerException();
+		/*
+		 * Si el elemento no esta en la lista se lanza la excepcion marcada en la
+		 * documentacion
+		 */
+		if (!this.contains(elem))
+			throw new NoSuchElementException();
 
+		if (this.size() == 1 && this.front.elem.equals(elem)) {
+			/* Si es el unico elemento de la lista se vacia y se devuelve 1 */
+			this.clear();
+			return 1;
+		}
+
+		/*
+		 * Si se ha llegado a este punto el elemento esta al menos una vez y la lista
+		 * tiene al menos un elemento por lo que se recorre de principio a fin buscando
+		 * esos elementos y eliminandolos incrementando el contador
+		 */
+		int apariciones = 0;
+		DoubleNode<T> aux = this.front;
+		while (aux != null) {
+			if (aux.elem.equals(elem)) {
+				/*
+				 * Si es el primer elemento se mueve el front, si no se elimina el elemento
+				 * saltandolo
+				 */
+				if (aux.prev == null)
+					this.front = this.front.next;
+				else
+					aux.prev.next = aux.next;
+				apariciones++;
+			}
+			aux = aux.next;
+		}
+		return apariciones;
+	}
 
 	@Override
 	public boolean contains(T elem) {
-		// TODO Auto-generated method stub
+		/*
+		 * Si el elemento a comparar es nulo se lanza la excepcion marcada en la
+		 * documentacion
+		 */
+		if (elem == null)
+			throw new NullPointerException();
+		/* Si la lista esta vacia no va a contener el elemento */
+		if (this.isEmpty())
+			return false;
+
+		/*
+		 * Se recorre la lista desde front a last con un do-while por si solo hay un
+		 * elemento
+		 */
+		DoubleNode<T> aux = this.front;
+		do {
+			/* Si se encuentra directamente se devuelve true */
+			if (aux.elem.equals(elem))
+				return true;
+			aux = aux.next;
+		} while (aux != this.last);
+		/*
+		 * Si se ha recorrido todo el bucle y no se ha encontrado ninguna coincidencia
+		 * se devuelve false
+		 */
 		return false;
 	}
 
-
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		/*
+		 * Se recorre de principio a fin la lista incrementado la variable a devolver en
+		 * cada paso
+		 */
+		int size = 0;
+		DoubleNode<T> aux = this.front;
+		while (aux != null) {
+			size++;
+			aux = aux.next;
+		}
+		return size;
 	}
-
 
 	@Override
 	public String toStringReverse() {
-		// TODO Auto-generated method stub
-		return null;
+		StringBuilder str = new StringBuilder("(");
+		/*
+		 * Se recorre del ultimo al primero añadiendo el toString del elemento
+		 * almacenado y un espacio
+		 */
+		DoubleNode<T> aux = this.last;
+		while (aux != null) {
+			str.append(aux.elem.toString()).append(" ");
+			aux = aux.prev;
+		}
+		str.append(")");
+		return str.toString();
 	}
 
 	@Override
@@ -194,13 +252,11 @@ public class DoubleLinkedListImpl<T> implements DoubleList<T> {
 		return null;
 	}
 
-
 	@Override
 	public int maxRepeated() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
 
 	@Override
 	public boolean isEquals(DoubleList<T> other) {
@@ -208,13 +264,11 @@ public class DoubleLinkedListImpl<T> implements DoubleList<T> {
 		return false;
 	}
 
-
 	@Override
 	public boolean containsAll(DoubleList<T> other) {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
 
 	@Override
 	public boolean isSubList(DoubleList<T> other) {
@@ -222,17 +276,26 @@ public class DoubleLinkedListImpl<T> implements DoubleList<T> {
 		return false;
 	}
 
-
 	@Override
 	public String toStringFromUntil(int from, int until) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-     return null;
+		StringBuilder str = new StringBuilder("(");
+		/*
+		 * Se recorre del primero al ultimo añadiendo el toString del elemento
+		 * almacenado y un espacio
+		 */
+		DoubleNode<T> aux = this.front;
+		while (aux != null) {
+			str.append(aux.elem.toString()).append(" ");
+			aux = aux.next;
+		}
+		str.append(")");
+		return str.toString();
 	}
 
 	@Override
@@ -247,19 +310,16 @@ public class DoubleLinkedListImpl<T> implements DoubleList<T> {
 		return null;
 	}
 
-
 	@Override
 	public Iterator<T> evenPositionsIterator() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-
 	@Override
 	public Iterator<T> progressIterator() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 
 }
